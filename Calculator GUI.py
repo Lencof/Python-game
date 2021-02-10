33bea6c51767a870a6fa5950330dfcc8234aad97
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-10 14:03:16 +0300

    Update Calculator GUI.py

--- Calculator GUI.py
+++ Calculator GUI.py
@@ -1,2 +1,11 @@
 # __Author__ __Lencof__
 # Calculator GUI.py
+ from tkinter import *
+  from operator import add, sub, mul, truediv
+  
+  OPFUNC = {'+': add, '-': sub, '*': mul, '/': truediv}
+  
+  class ParseErr(Exception):
+    pass
+  
+  # I will continue the project
