532a07e30b58c477dd59c45b8afb91e7c93f797e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-15 14:29:13 +0300

    Update Ping_pong.py

--- Ping_pong.py
+++ Ping_pong.py
@@ -1,1 +1,49 @@
+# __Author__ __Lencof__
+# Ping_pong.py
 
+import turtle as t
+import os
+
+# Score varibales
+
+player_a_score = 0
+player_b_score = 0
+
+win = t.Screen() # creating a window
+win.title("Ping-Pong Game") # Giving name to the game.
+win.bgcolor('black') # providing color to the HomeScreen
+win.setup(width=800,height=600) # Size of the game panel
+win.tracer(0) # which speed up's the game
+
+# Creating left paddie for the game
+
+paddle_left = t.Turtle()
+paddle_left.speed(0)
+paddle_left.shape('square')
+paddle_left.color('red')
+paddle_left.shapesize(stretch_wid=5,stretch_len=1)
+paddle_left.penup()
+paddle_left.goto(-350,0)
+
+# Creating a ringht paddie for the game
+
+paddle_right = t.Turtle()
+paddle_right.speed(0)
+paddle_right.shape('square')
+paddle_right.shapesize(stretch_wid=5,stretch_len=1)
+paddle_right.color('red')
+paddle_right.penup()
+paddle_right.goto(350,0)
+
+# Creating a pong ball for the game
+
+ball = t.Turtle()
+ball.speed(0)
+ball.shape('circle')
+ball.color('yellow')
+ball.penup()
+ball.goto(0,0)
+ball_dx = 1.5 # Setting up the pixels for the ball movement.
+all_by = 1.5
+
+# I'll add the code soon
