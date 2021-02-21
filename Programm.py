75c670ac7e0512141bbca9e235df9c1fa1bee9f1
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-21 08:47:05 +0300

    Update Programm.py

--- Programm.py
+++ Programm.py
@@ -1,2 +1,15 @@
 # __Author__ __Lencof__
 # Programm.py
+
+import sys # use sys
+from tkinter import * 
+from tkinter import filedialog # use filedialog
+
+def mHello():
+    mText=ment.get()
+    mlabel1 = Label = Label(myApp,text=mText).pack()
+    
+def myNew():
+    mlabel1 = Label(myApp,text="YO").pack()
+    
+# I will add the code
