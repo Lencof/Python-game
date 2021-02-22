906edc15ebbf0feaa9ec9c4ac3c20eebba717e7e
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-22 08:03:12 +0300

    Update Programm.py

--- Programm.py
+++ Programm.py
@@ -12,4 +12,28 @@ def mHello():
 def myNew():
     mlabel1 = Label(myApp,text="YO").pack()
     
+def myOpen():
+    myOpen = filedialog.askopenfile()
+    mlabe14 = Label(myApp,text=myOpen).pack()
+    
+def mAbout():
+    messagebox.showinfo(title="About",message="This is the about box")
+
+def mQuit():
+    mExit = messagebox.askyesno(title="Quit", message="Are you sure")
+    if mExit > 0:
+        myApp.destroy()    
+        
+        
+myApp = tk()
+# create a string variable
+ment = StringVar()
+
+# set the size of window
+myApp.geometry('450x450+200+200')
+
+myApp.title('MyApp')
+
+mLabel = Label(myApp,text='my label').pack()
+
 # I will add the code
