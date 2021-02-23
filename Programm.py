c7e741f6ee922d4cbc06f83e21328c1459d2cadb
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-23 10:44:12 +0300

    Update Programm.py

--- Programm.py
+++ Programm.py
@@ -39,3 +39,33 @@ mLabel = Label(myApp,text='my label').pack()
 mButton = Button(myApp,text = 'OK', command = mHello).pack()
 
 #set the ment variable from the text entry box
+nEntry = Entry(myApp,textvariable=ment).pack()
+
+# create the menubar
+menubar = Menu(myApp)
+
+# create the file component of the menubar
+filemenu = Menu(menubar, tearoff=0)
+
+# Add the sub headings to the file menu
+filemenu.add_command(Label="New", command=myNew)
+filemenu.add_command(Label="Open", command=myOpen)
+filemenu.add_command(Label="Save As...")
+filemenu.add_command(Label="Close", command=mQuit)
+
+menubar.add_cascade(Label="File",menu=filemenu)
+
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(Label+"Help docs")
+helpmenu.add_command(Label="About",command=mAbout)
+menubar.add_cascade(Label="Help",menu=helpmenu)
+
+
+# add menubarr to the window
+myApp.config(menu=menubar)
+
+myApp.mainloop() # close
+
+
+
